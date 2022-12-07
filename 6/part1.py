09e30fda15ce02b3e94f8fc545ee493b42da23d3
Karlsson <qwertderStar@gmail.com>
2022-12-07 23:55:00 +0100

    day 6

---
+++ 6/part1.py
@@ -0,0 +1,12 @@
+n = 4
+
+def main():
+    with open("./input.txt", "r") as stream:
+        stream = stream.read()
+        for i in range(len(stream)):
+            if len(list(set(stream[i:i+n]))) >= n:
+                print(i+n)
+                return 0
+
+if __name__ == "__main__":
+    main()
